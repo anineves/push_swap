476b92b7d4f0e2a539f60d1e00b04c0f6eb59ab3
Ana Isabel Neves <85195243+anineves@users.noreply.github.com>
2023-03-31 17:22:40 +0100

    Update sort_100.c

--- sort_100.c
+++ sort_100.c
@@ -1,90 +1,122 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_100.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asousa-n <asousa-n@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/30 15:06:07 by asousa-n          #+#    #+#             */
+/*   Updated: 2023/03/30 15:06:09 by asousa-n         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
-void	ft_sort_b(t_stack *stack_a, t_stack *stack_b)
+void	ft_sort_b(t_stack *stack_a, t_stack *stack_b, int i)
 {
 	int	higher;
-	int	midle;
-	int 	pos;
-	
-	pos = 0;
-	while(stack_b->size > 0)
+
+	higher = 0;
+	while (i > 0)
 	{
-		midle =  stack_b->size / 2;
 		higher = ft_find_higher(stack_b);
-		pos = ft_get_position(stack_b, higher);
-	
-		if(pos < midle)
+		while (ft_get_position(stack_b, higher) != 0)
 		{
-			while(pos > 0)
-			{
-				
+			if (ft_get_position(stack_b, higher) < (stack_b->size / 2))
 				do_rb(stack_b);
-				pos--;
-			}
-		}
-		else
-		{
-			while(pos <= stack_b->size - 1)
-			{
+			else
 				do_rrb(stack_b);
-				pos++;
-			}
 		}
 		do_pa(stack_b, stack_a);
+		i--;
 	}
 }
 
-void quicksort(int stack[], int tam) 
-{  
-    	int	i;
+void	ft_quicksort(int *copy, int size)
+{
+	int	i;
 	int	tmp;
 
 	i = 0;
-	while (i < tam)
+	while (i < size -1)
 	{
-		if (stack[i] > stack[i + 1])
+		if (copy[i] > copy[i + 1])
 		{
-			tmp = stack[i];
-			stack[i] = stack[i + 1];
-			stack[i + 1] = tmp;
+			tmp = copy[i];
+			copy[i] = copy[i + 1];
+			copy[i + 1] = tmp;
 			i = -1;
 		}
 		i++;
 	}
 }
 
-int ft_mediana(t_stack *stack_a, int size)
+int	ft_mediana(t_stack *stack_a)
 {
-	int mediana;
-	
+	int	mediana;
+	int	*tab;
+	int	i;
+
+	i = 0;
 	mediana = 0;
-	
-	quicksort(stack_a->copy , stack_a->size-1);
-	stack_a->blocks = stack_a->blocks +(size / 20);
-	mediana = stack_a->copy[stack_a->blocks];
-	 
-	 return(mediana);
-		
+	tab = malloc(sizeof(int) * stack_a->size);
+	while (i < stack_a->size)
+	{
+		tab[i] = stack_a->stack[i];
+		i++;
+	}
+	ft_quicksort(tab, stack_a->size);
+	mediana = tab[stack_a->blocks];
+	free(tab);
+	return (mediana);
 }
+
 void	ft_sort_100(t_stack *stack_a, t_stack *stack_b)
 {
 	int	med;
-	int size;
-	
-	size = stack_a->size;
-	stack_a->blocks = 0;
-	while (stack_a->size > 5)
+	int	i;
+
+	stack_a->blocks = 20;
+	i = 0;
+	while (stack_a->size > 20)
 	{
-		med = ft_mediana(stack_a, size);	
+		med = ft_mediana(stack_a);
 		while (ft_find_smaller(stack_a) < med)
 		{
 			if (stack_a->stack[0] < med)
+			{
 				do_pb(stack_a, stack_b);
+				i++;
+			}
 			else
 				do_ra(stack_a);
 		}
-
 	}
-	ft_sort_five(stack_a, stack_b);
-	ft_sort_b(stack_a, stack_b);
+	ft_sort_20(stack_a, stack_b);
+	ft_sort_b(stack_a, stack_b, i);
+}
+
+void	ft_sort_500(t_stack *stack_a, t_stack *stack_b)
+{
+	int	med;
+	int	 i;
+
+	i = 0;
+	stack_a->blocks = 45;
+	while (stack_a->size > 100)
+	{
+		med = ft_mediana(stack_a);
+		while (ft_find_smaller(stack_a) < med)
+		{
+			if (stack_a->stack[0] < med)
+			{
+				do_pb(stack_a, stack_b);
+				i++;
+			}
+			else
+				do_ra(stack_a);
+		}
+	}
+	ft_sort_100(stack_a, stack_b);
+	ft_sort_b(stack_a, stack_b, i);
 }
